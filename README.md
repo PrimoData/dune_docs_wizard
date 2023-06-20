f5b9a05faf2407d401d15a8df344b602c554c9ce
Primo Data <mershon.max16@gmail.com>
2023-06-20 02:58:09 +0000

    small things

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Sources:
 Inspirations:
 * [How I Built a Custom GPT-Based Chatbot in Under 10 Minutes with LlamaIndex](https://towardsdatascience.com/how-i-built-a-custom-gpt-based-chatbot-in-under-10-minutes-with-llamaindex-2102f0173420)
 * [A step-by-step guide to building a chatbot based on your own documents with GPT](https://bootcamp.uxdesign.cc/a-step-by-step-guide-to-building-a-chatbot-based-on-your-own-documents-with-gpt-2d550534eea5)
+* [LlamaHub Loaders](https://llama-hub-ui.vercel.app/)
--- app.py
+++ app.py
@@ -1,85 +1,31 @@
 import streamlit as st
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
 import os
-from pandasai import PandasAI
-from pandasai.llm.openai import OpenAI
-import pandas as pd
-import requests
-import json
-import re
-
-# Configure Streamlit Page
-page_icon = "https://dune.com/assets/DuneLogoCircle.svg"
-st.set_page_config(page_title="Dune AI Analyst", page_icon=page_icon, layout="wide")
-
-# Read Custom CSS
-with open("assets/css/style.css", "r") as f:
-    css_text = f.read()
-custom_css = f"<style>{css_text}</style>"
-st.markdown(custom_css, unsafe_allow_html=True)
-
-# OpenAI "powered by" component
-powered_by = """
-<div style="display: flex; align-items: center; margin-bottom:20px">
-    <span style="font-size: 14px; margin-right: 4px; font-style: italic;">Powered by:</span>
-    <img src="https://www.freelogovectors.net/wp-content/uploads/2023/01/openai-logo-freelogovectors.net_.png" alt="OpenAI logo" height="24">
-</div>
-"""
-
-# Get API Keys
-openai_api_key = os.environ["OPENAI_KEY"]
-dune_api_key = os.environ["DUNE_KEY"]
-
-# Heading
-st.write(
-    f'<h1><img src="https://dune.com/assets/DuneLogoCircle.svg" alt="OpenAI logo" height="36" style="margin-bottom:6px">  Dune AI Analyst</h1>',
-    unsafe_allow_html=True,
-)
-st.subheader("Analyze any Dune dataset using natural language")
-st.markdown(powered_by, unsafe_allow_html=True)
+import openai
 
 
-# Query Dune Analytics using API
-def query_dune(q):
-    url = f"https://api.dune.com/api/v1/query/{q}/results?api_key={dune_api_key}"
-    response = requests.get(url)
-    results_json = json.loads(response.text)["result"]["rows"]
-    results_df = pd.DataFrame.from_dict(results_json)
-    return results_df
+@st.cache_resource
+def load_indexes():
+    """load the pipeline object for preprocessing and the ml model"""
 
+    # Loading Index from local storage
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)
+    return index
 
-# Initialize Session State
-if "df" not in st.session_state:
-    st.session_state["df"] = ""
+# api key
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
-col1, col2 = st.columns(2)
+# load indices
+index = load_indexes()
 
-# Query Dune
-with col1:
-    query = st.text_input("(1) Enter a Dune Query Id.")
-    submit_query = st.button("Query Data")
-    if submit_query:
-        st.session_state.df = query_dune(query)
-        st.table(st.session_state.df.head())
-    elif isinstance(st.session_state.df, pd.DataFrame):
-        st.table(st.session_state.df.head())
+st.header('Dune Docs Wizard')
 
-# Ask Question
-with col2:
-    # Setup Pandas AI
-    llm = OpenAI(openai_api_key)
-    pandas_ai = PandasAI(llm, save_charts=True)
+# query the selected index
+query = st.text_input('Enter Your Query')
+button = st.button(f'Response')
 
-    # Analyze Data
-    question = st.text_input("(2) Ask a question or plot results.")
-    submit_question = st.button("Analyze Data")
-
-    if submit_question:
-        answer = pandas_ai(st.session_state.df, prompt=question)
-        try:
-            # Display image if generated
-            pattern = r"Charts saved to: (.*)"
-            file_path = re.search(pattern, answer).group(1)
-            st.image(f"{file_path}/chart.png")
-        except:
-            # Otherwise display text
-            st.write("Answer:", answer)
+if button:
+    query_engine = index.as_query_engine()
+    response = query_engine.query(query)
+    st.write(response)
