815f7bd233e55273588b43004f0cb53278eb505e
Primo Data <mershon.max16@gmail.com>
2023-06-21 13:25:35 +0000

    proof of concept done

--- README.md
+++ README.md
@@ -5,11 +5,13 @@
 [![Code style: black](https://img.shields.io/badge/Code%20style-black-000000.svg)](https://github.com/psf/black)
 
 
-Sources:
-* [Dune Docs GitHub Repo](https://github.com/duneanalytics/docs)
-* [Dune Docs Website](https://dune.com/docs/)
+Ask any question about Dune's [technical docs](https://dune.com/docs/).
 
-Inspirations:
+:hammer: Built with:
+* [LlamaIndex](https://llamalab.com/llamaindex/)
+* [OpenAI API](https://beta.openai.com/)
+* [Streamlit](https://streamlit.io/)
+
+:star: Inspirations:
 * [How I Built a Custom GPT-Based Chatbot in Under 10 Minutes with LlamaIndex](https://towardsdatascience.com/how-i-built-a-custom-gpt-based-chatbot-in-under-10-minutes-with-llamaindex-2102f0173420)
 * [A step-by-step guide to building a chatbot based on your own documents with GPT](https://bootcamp.uxdesign.cc/a-step-by-step-guide-to-building-a-chatbot-based-on-your-own-documents-with-gpt-2d550534eea5)
-* [LlamaHub Loaders](https://llama-hub-ui.vercel.app/)
--- app.py
+++ app.py
@@ -1,28 +1,51 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, GPTVectorStoreIndex
+from llama_index import StorageContext, load_index_from_storage
 import os
 import openai
-import pickle
+
+
+# Configure Streamlit Page
+page_icon = "https://dune.com/assets/DuneLogoCircle.svg"
+st.set_page_config(page_title="Dune Docs Wizard", page_icon=page_icon)
+
+# Read Custom CSS
+with open("assets/css/style.css", "r") as f:
+    css_text = f.read()
+custom_css = f"<style>{css_text}</style>"
+st.markdown(custom_css, unsafe_allow_html=True)
+
+# Headings
+st.write(
+    '<h1><img src="https://dune.com/assets/DuneLogoCircle.svg" alt="OpenAI logo" height="36" style="margin-bottom:6px">  Dune Docs Wizard</h1>',
+    unsafe_allow_html=True,
+)
+st.write(
+    "<h5>Ask any question about Dune's <a href='https://dune.com/docs/'>technical docs</a>.</h5>",
+    unsafe_allow_html=True,
+)
+
+# Example
+with st.expander("Example Question:"):
+    st.code("What are Spellbooks?")
+
 
 @st.cache_resource
 def load_indexes():
     # Loading Index from local storage
-    with open("dune_docs.pkl", "rb") as f:
-        docs = pickle.load(f)
-    index = GPTVectorStoreIndex.from_documents(docs)
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)
     return index
 
-# api key
+
+# API Key
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# load indices
+# Load index
 index = load_indexes()
 
-st.header('Dune Docs Wizard')
-
-# query the selected index
-query = st.text_input('Enter Your Query')
-button = st.button(f'Response')
+# Query index
+query = st.text_input("Question", help="")
+button = st.button("Answer")
 
 if button:
     query_engine = index.as_query_engine()
